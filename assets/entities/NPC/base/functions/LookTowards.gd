7434b104a19f04d55edaf0e011f33884fee976ca
Oliver Andrew Rose <oliver.rose898@gmail.com>
2021-10-30 02:24:10 +0100

    Pub & smoothed movement.
    Added the NPC to interpolate when looking towards targets.
    
    Also added a pub to the area.

--- assets/entities/NPC/base/functions/LookTowards.gd
+++ assets/entities/NPC/base/functions/LookTowards.gd
@@ -3,5 +3,13 @@ class_name LookTowards
 
 var look_toward: Vector3;
 
+
 func look_towards(direction: Vector3):
+	look_toward = direction;
+	pass
+	
+func _process(delta):
+	var target_transform = owner.get_global_transform().looking_at(look_toward, Vector3.UP);
+	owner.global_transform = owner.get_global_transform().interpolate_with(target_transform, delta*4);
+	#owner.look_at(look_toward, Vector3.UP);
 	pass
